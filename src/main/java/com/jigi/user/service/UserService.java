2b061a8ba7e42d29fe682cb12c96c9f2ff1f4709
ziggy-jigi <sonbj722@gmail.com>
2022-10-30 22:57:27 +0900

    오류는 해결..

--- src/main/java/com/jigi/user/service/UserService.java
+++ src/main/java/com/jigi/user/service/UserService.java
@@ -5,16 +5,8 @@ import com.jigi.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.io.InputStream;
 
-@Service
-public class UserService {
-    @Autowired
-    private UserMapper userMapper;
+public interface UserService {
+    void insertUser(UserDto userDto) throws Exception;
 
-
-    public void insertUser(UserDto userDto) throws Exception {
-        userMapper.insertUser(userDto);
-    }
 }
---
+++ src/main/java/com/jigi/user/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.jigi.user.service;
+
+import com.jigi.user.dto.UserDto;
+import com.jigi.user.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Component
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public void insertUser(UserDto userDto) throws Exception {
+        userService.insertUser(userDto);
+    }
+}
