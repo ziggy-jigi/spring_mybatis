35e8457be703d866b58dc38235c27782106702db
Byungjik <sonbj722@gmail.com>
2022-10-30 20:12:46 +0900

    여전히 오류

--- src/main/java/com/jigi/user/controller/UserController.java
+++ src/main/java/com/jigi/user/controller/UserController.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @Autowired
-    UserService userService;
+    private UserService userService;
 
     @RequestMapping("/api/userInsert")
-    public String insertUser(UserDto usertDto){
+    public String insertUser(UserDto usertDto) throws Exception {
         System.out.println("/api/goUserInsert 요청은 들어오니..? 나중엔 log를 찍을거에요");
         userService.insertUser(usertDto);
         return "user/userInsertAfter";
--- src/main/java/com/jigi/user/service/UserService.java
+++ src/main/java/com/jigi/user/service/UserService.java
@@ -3,16 +3,18 @@ package com.jigi.user.service;
 import com.jigi.user.dto.UserDto;
 import com.jigi.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @Service
 public class UserService {
-
     @Autowired
-    UserMapper userMapper;
+    private UserMapper userMapper;
 
-    public void insertUser(UserDto userDto){
+
+    public void insertUser(UserDto userDto) throws Exception {
         userMapper.insertUser(userDto);
     }
 }
