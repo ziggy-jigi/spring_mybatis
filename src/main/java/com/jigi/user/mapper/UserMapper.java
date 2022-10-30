d24b14f95a25a2183071a0768c446326db8f7d5a
ziggy-jigi <sonbj722@gmail.com>
2022-10-31 06:32:51 +0900

    @Mapper 어노테이션 제거

--- src/main/java/com/jigi/user/mapper/UserMapper.java
+++ src/main/java/com/jigi/user/mapper/UserMapper.java
@@ -4,7 +4,7 @@ import com.jigi.user.dto.UserDto;
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 
-@Mapper
+
 public interface UserMapper {
     UserDto getUserById(String user_email);
     public void insertUser(UserDto userDto) throws Exception;
